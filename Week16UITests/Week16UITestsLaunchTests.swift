9df7b7d40c2471b22482a7bdb4b29ce5a85f26fd
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-01-28 13:03:28 +0300

    Initial Commit

---
+++ Week16UITests/Week16UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Week16UITestsLaunchTests.swift
+//  Week16UITests
+//
+//  Created by Mehmet Salih ÇELİK on 28.01.2022.
+//
+
+import XCTest
+
+class Week16UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
